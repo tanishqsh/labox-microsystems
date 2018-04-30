ce8ede34c5a26b03a6905e07fd3cb42f0d21e772
tanishqsh <tanishqsh@gmail.com>
2018-04-30 13:12:48 +0530

    updated dependencies

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,19 +8,18 @@ const Header = props => (
         <div className={'container-fluid primary-fill topBar'}/>
         <div className={'container header-container'}>
             <div className={'row hidden-lg'}>
-                <div className={'col-xs-2 onTop'}>
+                <div className={'col-xs-2 onTop mobileOnly component-wrapper'}>
                     <OffCanvasMenu/>
                 </div>
                 <div className={'col-xs-10 col-lg-6 onBottom'}>
                     <Link to={'/'}>
                         <div className={'logo-container'}>
-                            LABO<span className={'sup-element'}>&reg;</span><br/>
+                            LABO <span className={'sup-element'}>&reg;</span><br/>
                             <div className={'logo-sub-title'}> Worlds Within </div>
-
                         </div>
                     </Link>
                 </div>
-                <div className={'col-lg-6 text-right mobileHide'}>
+                <div className={'col-xs-2 col-lg-6 text-right mobileHide'}>
                     <div className={'menu-container'}>
                         <Link to={'/'} className={'menu-item'}> Home </Link>
                         <Link to={'/products'} className={'menu-item'}> Products </Link>
